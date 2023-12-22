4d273dba28b05ed796053e600011080125bd30bd
Vinicius Costa <54358510+ViniciusCosta126@users.noreply.github.com>
2023-12-22 18:46:28 -0300

    Tratamento de erros

--- backend/src/controllers/NotaController.js
+++ backend/src/controllers/NotaController.js
@@ -4,20 +4,31 @@ import Nota from "../models/Nota.js"
 class NotaController {
     static async listarNotas(req, res, next) {
         try {
-            const dados = await Nota.findAll().then(dados => dados).catch(erro => console.log(erro))
-            res.status(200).send(dados)
+            const dados = await Nota.findAll()
+            if (dados.length > 0) {
+                res.status(200).json(dados)
+            } else {
+                res.status(404).send({ 'message': "Nenhum dado encontrado!!" })
+            }
+
         } catch (error) {
             console.log(error)
         }
     }
-    static async listarNotasPorCliente(req,res,next){
+    static async listarNotasPorCliente(req, res, next) {
         try {
-            const {numero_cliente} = req.params
-            console.log(numero_cliente)
-            const dados = await Nota.findAll({where:{
-                numero_cliente: numero_cliente
-            }})
-            res.status(200).send(dados)
+            const { numero_cliente } = req.params
+            const dados = await Nota.findAll({
+                where: {
+                    numero_cliente: numero_cliente
+                }
+            })
+            if (dados.length > 0) {
+                res.status(200).json(dados)
+            } else {
+                res.status(404).send({ 'message': "Cliente nao encontrado!!" })
+            }
+
         } catch (erro) {
             console.log(erro)
         }
